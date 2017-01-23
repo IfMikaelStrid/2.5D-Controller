3cf41d362837f2a004774c1e1b537f07aef1878c
IfMikaelStrid <stridmikael@gmail.com>
2017-01-23 07:34:51 +0100

    CameraFix
    follow fix

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,30 +1,43 @@
 ﻿using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
-    public Transform player;
-    float panConstantX, playerUpdatePos, posUpdate;
+    //public Transform player;
+    //float panConstantX, playerUpdatePos, posUpdate;
+    public GameObject player;
+    public float offset,offsetSmoothing;
+    private Vector3 playerPosition;
     private void Start()
     {
-        transform.position = new Vector3(player.position.x, transform.position.y, -20);
-        panConstantX = Mathf.Abs(player.position.x) + 5;
-        playerUpdatePos = Mathf.Abs(player.position.x);
+        //transform.position = new Vector3(player.position.x, transform.position.y, -20);
+        //panConstantX = Mathf.Abs(player.position.x) + 5;
+        //playerUpdatePos = Mathf.Abs(player.position.x);
     }
     void Update() {
-        if (Mathf.Abs(player.position.x) > panConstantX)
+        playerPosition = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        if (player.transform.localScale.x > 0f)
         {
-            if (player.position.x > playerUpdatePos)
-            {
-                transform.position = new Vector3(player.position.x - 5, transform.position.y, -20);
-                //if (player.position.x == posUpdate)
-                //{
-
-                //}
-            }
-            else if (player.position.x < playerUpdatePos)
-            {
-                transform.position = new Vector3(player.position.x + 5, transform.position.y, -20);
-            }
+            playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, playerPosition.z);
         }
-        posUpdate = player.position.x;
+        else
+        {
+            playerPosition = new Vector3(playerPosition.x - offset, playerPosition.y, playerPosition.z);
+        }
+        transform.position = Vector3.Lerp(transform.position, playerPosition, offsetSmoothing * Time.deltaTime);
+        //if (Mathf.Abs(player.position.x) > panConstantX)
+        //{
+        //    if (player.position.x > playerUpdatePos)
+        //    {
+        //        transform.position = new Vector3(player.position.x - 5, transform.position.y, -20);
+        //        //if (player.position.x == posUpdate)
+        //        //{
+
+        //        //}
+        //    }
+        //    else if (player.position.x < playerUpdatePos)
+        //    {
+        //        transform.position = new Vector3(player.position.x + 5, transform.position.y, -20);
+        //    }
+        //}
+        //posUpdate = player.position.x;
     }
 }
